e893fa473e111c4c68cc0753cf9e0935c9edd360
Taisuke Fukuno <fukuno@jig.jp>
2024-01-15 23:01:16 +0800

    add Cache / fetchOrLoad

--- Cache.js
+++ Cache.js
@@ -11,12 +11,20 @@ export class Cache {
   async fetchOrLoad(url) {
     const res = await this.db.get("url", url);
     if (res.length > 0) {
-      console.log("cached");
       return res[0].data;
     }
-    console.log("fetch");
     const res2 = new Uint8Array(await (await fetch(url)).arrayBuffer());
     await this.db.add({ url, data: res2 });
     return res2;
   }
 }
+
+let cache = null;
+
+export const fetchOrLoad = async (url) => {
+  if (!cache) {
+    cache = await Cache.create();
+  }
+  const bin = await cache.fetchOrLoad(url);
+  return bin;
+};
